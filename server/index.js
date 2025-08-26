760e1f753abf80a16ccc886b3862fe4d689db0ee
Rodrigo Arias <rarias10@gmail.com>
2025-08-26 14:17:23 -0600

    Changes in Server

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/api/hello", (req, res) => {
-  res.json({ message: "Wow!" });
+  res.json({ message: "Wow! This is amazing" });
 });
 
 app.listen(5000, () => console.log("Server running on http://localhost:5000"));
